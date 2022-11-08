f6d2aaeccae367c326fd619a0f432de451c8ea1f
alisa <baliiska@outlook.com>
2022-11-08 22:29:14 +0700

    первый и второй слайды, регистрация на кнопке

--- src/js/script.js
+++ src/js/script.js
@@ -13,12 +13,28 @@ $(document).ready(function(){
             email: "Корейцы сперли Вашу собаку??"
         }
     });
+    
 
+    $("#EButton").on('click', function(){
+        $('.overlay,#Thanks').fadeIn();
+    })
     $("[data-modal=modal_close]").on('click', function(){
         $('.overlay,#Thanks').fadeOut();
     })
 
-    $('#EForm').on("submit", function(e){
+
+    $("#ZaEButton").on('click', function(){
+        $('.Eoverlay,#Regist').fadeIn();
+    })
+    $("#ZaeeEButton").on('click', function(){
+        $('.Eoverlay,#Regist').fadeOut();
+        $('.overlay,#Thanks').fadeIn();
+    })
+
+
+
+
+    /*$("#EButton").on('click', function(e){
 
         e.preventDefault();      //отменить стандарт.поведен браузера (после отправки формы
                                     //перезагрузка страницы произвоиться не будет)
@@ -32,5 +48,5 @@ $(document).ready(function(){
             $('RegistInput').trigger('reset');
         })
         return false;
-    })
+    })*/
 })
